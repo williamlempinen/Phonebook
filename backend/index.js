f22c00ad3a4d2c4a0c29c08b0fb5c7b7cecbf5b7
William Lempinen <wlempin@gmail.com>
2023-02-14 18:04:37 +0200

    3.3 done

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,17 @@ app.get("/api/info", (req, res) => {
     res.send(info + "<br />" +  date);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
