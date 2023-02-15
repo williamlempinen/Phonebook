4f730703d0dba2942efa5b63aab8ff5ff6b7f706
William Lempinen <wlempin@gmail.com>
2023-02-15 18:04:56 +0200

    3.5 done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+app.use(express.json());
 
 let persons = [
     {
@@ -52,11 +53,40 @@ app.get('/api/persons/:id', (req, res) => {
     }
 });
 
-app.delete("api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter(person => person.id !== id);
 
   res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const newId = (Math.random() * 1000).toFixed(0);
+  const body = req.body;
+  
+  if (!body.content) {
+    return (
+      res.status(400).json({
+        error: "content missing"
+      })
+    );
+  } else if (body.name) {
+
+  } else {
+    const newPerson = {
+      id: newId,
+      name: body.name,
+      number: body.number
+    };
+
+    persons = persons.concat(newPerson);
+
+    res.json(newPerson);
+    console.log(req.headers);
+  }
+
+  console.log(newPerson);
+  res.json(newPerson);
 });
 
 const PORT = 3001
